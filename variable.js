a0355a1c31566824c15f10e282112c623bedf4d2
maya shingne <mayamhaske1722@gmail.com>
2022-11-08 15:41:04 +0530

    add variable.js

---
+++ variable.js
@@ -0,0 +1,6 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "you are eligible";
+var s2 = "you are to late";
+console.log(s2);
+console.log(s1);
---
+++ variable.ts
@@ -0,0 +1,7 @@
+var n1:number=10;
+console.log(n1);
+ var s1:string= "you are eligible";
+ var s2:string="you are to late";
+ console.log(s2);
+ console.log(s1);
+ 
